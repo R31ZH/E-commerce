23ed27577527e5cb5360fe1def34614256f508d4
jaguilar0047@gmail.com <Jaguilar0047@gmail.com>
2022-08-29 21:38:17 -0700

    save2

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,6 +6,18 @@ const { Category, Product } = require("../../models");
 router.get("/", (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes: ["id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["product_name", "price", "stock", "category_id"],
+      },
+    ],
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get("/:id", (req, res) => {
